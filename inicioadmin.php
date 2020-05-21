a94e77855fa59a1ec78dcc992555160e5108e49e
Camilo R <riveroscamilo@javeriana.edu.co>
2020-05-20 21:26:55 -0500

    Inicio Admin
    Modelo para el modulo admin

---
+++ inicioadmin.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>index</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</head>
+<style type="text/css">
+  body {
+    background:url(https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTKW1ip1s-T9tW2R4-X5mD49OTbr9oNg28uJg2Iy-sc4XgDdMli&usqp=CAU) ;
+   background-repeat: no-repeat, repeat;
+   background-size: 1600px;
+  }
+    
+</style>
+
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Sistema medico</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Crear Administrador<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Agregar habitaciones</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Agregar camas</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Lista de Pacientes</a>
+      </li>
+    </ul>
+    <span class="navbar-text">
+      Cerrar sesion
+    </span>
+  </div>
+</nav>
+<div class="jumbotron text-center">
+  <h1>Bienvenido ADMINISTRADOR</h1>
+</div>
+  
+<div class="container">
+  <div class="row">
+    <div class="col-md-6">
+        <button type="button" class="btn btn-primary btn-lg btn-block">Mensajes</button>
+    </div>
+    <div class="col-md-6">
+        <button type="button" class="btn btn-secondary btn-lg btn-block">Lista de Medicos</button>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
