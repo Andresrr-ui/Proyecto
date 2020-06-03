4b81d40b2f7820501e6cd0eb180ed363e0bd93d9
Camilo R <riveroscamilo@javeriana.edu.co>
2020-06-03 17:58:31 -0500

    Update web.sql

--- DB/web.sql
+++ DB/web.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-06-2020 a las 03:30:49
+-- Tiempo de generación: 04-06-2020 a las 00:40:00
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.4.5
 
@@ -29,16 +29,16 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `admin` (
   `usuario` varchar(30) NOT NULL,
-  `apellido` varchar(30) NOT NULL,
-  `password` varchar(30) NOT NULL
+  `password` varchar(200) NOT NULL,
+  `rol` int(11) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `admin`
 --
 
-INSERT INTO `admin` (`usuario`, `apellido`, `password`) VALUES
-('admin', 'admin', 'admin');
+INSERT INTO `admin` (`usuario`, `password`, `rol`) VALUES
+('admin', '$2y$10$VxLQBC0xQtsJPkvUFzSOv.LBBPUG4yCdGqYEtc9HfCIp10OEVFHgu', 1);
 
 -- --------------------------------------------------------
 
@@ -47,9 +47,26 @@ INSERT INTO `admin` (`usuario`, `apellido`, `password`) VALUES
 --
 
 CREATE TABLE `camas` (
-  `codigo_hab` int(11) NOT NULL,
-  `numero` int(11) NOT NULL
+  `numero` int(11) NOT NULL,
+  `habitacion` int(11) NOT NULL,
+  `estado` int(11) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `camas`
+--
+
+INSERT INTO `camas` (`numero`, `habitacion`, `estado`) VALUES
+(1, 301, 1),
+(2, 301, 1),
+(3, 301, 1),
+(4, 301, 1),
+(5, 301, 1),
+(1, 302, 1),
+(2, 302, 1),
+(3, 302, 1),
+(1, 304, 1),
+(2, 304, 1);
 
 -- --------------------------------------------------------
 
@@ -63,6 +80,15 @@ CREATE TABLE `equipos_medicos` (
   `cantidad` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `equipos_medicos`
+--
+
+INSERT INTO `equipos_medicos` (`id`, `nombre`, `cantidad`) VALUES
+(1, 'Maquina_Ecografia', 21),
+(2, 'Respirador', 75),
+(4, 'desfribilador', 4);
+
 -- --------------------------------------------------------
 
 --
@@ -74,6 +100,15 @@ CREATE TABLE `habitacion` (
   `numero_cama` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `habitacion`
+--
+
+INSERT INTO `habitacion` (`codigo`, `numero_cama`) VALUES
+(301, 5),
+(302, 3),
+(304, 2);
+
 -- --------------------------------------------------------
 
 --
@@ -82,10 +117,18 @@ CREATE TABLE `habitacion` (
 
 CREATE TABLE `medico` (
   `id` int(11) NOT NULL,
-  `username` varchar(30) NOT NULL,
+  `username` varchar(30) DEFAULT NULL,
   `email` varchar(30) NOT NULL,
-  `password` int(11) NOT NULL
+  `password` varchar(200) NOT NULL,
+  `rol` int(11) NOT NULL DEFAULT 2
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `medico`
+--
+
+INSERT INTO `medico` (`id`, `username`, `email`, `password`, `rol`) VALUES
+(1, 'medico', 'medico@medico.com', '1234', 2);
 
 -- --------------------------------------------------------
 
@@ -99,9 +142,16 @@ CREATE TABLE `paciente` (
   `habitacion` int(11) NOT NULL,
   `cama` int(11) NOT NULL,
   `medico` int(11) NOT NULL,
-  `recursos` int(11) NOT NULL,
-  `equipos` int(11) NOT NULL
+  `recursos` int(11) DEFAULT NULL,
+  `equipos` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `paciente`
+--
+
+INSERT INTO `paciente` (`id`, `nombre`, `habitacion`, `cama`, `medico`, `recursos`, `equipos`) VALUES
+(2, 'prueba', 301, 1, 1, NULL, 4);
 
 -- --------------------------------------------------------
 
@@ -117,6 +167,13 @@ CREATE TABLE `peticion` (
   `equipo` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `peticion`
+--
+
+INSERT INTO `peticion` (`id`, `doctor`, `paciente`, `descripcion`, `equipo`) VALUES
+(1, 1, 2, 'Prueba de sangre', 4);
+
 -- --------------------------------------------------------
 
 --
@@ -130,14 +187,32 @@ CREATE TABLE `recursos` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Volcado de datos para la tabla `recursos`
+--
+
+INSERT INTO `recursos` (`id`, `nombre`, `cantidad`) VALUES
+(1, 'jeringa', 100),
+(2, 'anestecia', 13),
+(3, 'guantes', 15),
+(4, 'mascarillas', 85),
+(5, 'suero', 132);
+
+--
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`usuario`);
 
 --
 -- Indices de la tabla `camas`
 --
 ALTER TABLE `camas`
-  ADD KEY `codigo_hab` (`codigo_hab`);
+  ADD KEY `habitacion` (`habitacion`),
+  ADD KEY `numero` (`numero`) USING BTREE;
 
 --
 -- Indices de la tabla `equipos_medicos`
@@ -155,7 +230,9 @@ ALTER TABLE `habitacion`
 -- Indices de la tabla `medico`
 --
 ALTER TABLE `medico`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `username` (`username`),
+  ADD KEY `password` (`password`);
 
 --
 -- Indices de la tabla `paciente`
@@ -165,7 +242,8 @@ ALTER TABLE `paciente`
   ADD KEY `medico` (`medico`),
   ADD KEY `habitacion` (`habitacion`),
   ADD KEY `recursos` (`recursos`),
-  ADD KEY `equipos` (`equipos`);
+  ADD KEY `equipos` (`equipos`),
+  ADD KEY `cama` (`cama`);
 
 --
 -- Indices de la tabla `peticion`
@@ -190,31 +268,31 @@ ALTER TABLE `recursos`
 -- AUTO_INCREMENT de la tabla `equipos_medicos`
 --
 ALTER TABLE `equipos_medicos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `medico`
 --
 ALTER TABLE `medico`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `paciente`
 --
 ALTER TABLE `paciente`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `peticion`
 --
 ALTER TABLE `peticion`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `recursos`
 --
 ALTER TABLE `recursos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Restricciones para tablas volcadas
@@ -224,7 +302,7 @@ ALTER TABLE `recursos`
 -- Filtros para la tabla `camas`
 --
 ALTER TABLE `camas`
-  ADD CONSTRAINT `camas_ibfk_1` FOREIGN KEY (`codigo_hab`) REFERENCES `habitacion` (`codigo`);
+  ADD CONSTRAINT `camas_ibfk_1` FOREIGN KEY (`habitacion`) REFERENCES `habitacion` (`codigo`);
 
 --
 -- Filtros para la tabla `paciente`
@@ -233,7 +311,8 @@ ALTER TABLE `paciente`
   ADD CONSTRAINT `paciente_ibfk_1` FOREIGN KEY (`medico`) REFERENCES `medico` (`id`),
   ADD CONSTRAINT `paciente_ibfk_2` FOREIGN KEY (`habitacion`) REFERENCES `habitacion` (`codigo`),
   ADD CONSTRAINT `paciente_ibfk_3` FOREIGN KEY (`recursos`) REFERENCES `recursos` (`id`),
-  ADD CONSTRAINT `paciente_ibfk_4` FOREIGN KEY (`equipos`) REFERENCES `equipos_medicos` (`id`);
+  ADD CONSTRAINT `paciente_ibfk_4` FOREIGN KEY (`equipos`) REFERENCES `equipos_medicos` (`id`),
+  ADD CONSTRAINT `paciente_ibfk_5` FOREIGN KEY (`cama`) REFERENCES `camas` (`numero`);
 
 --
 -- Filtros para la tabla `peticion`
